d260ba3d095f08988b71f04322adb26004035270
Caleb Gardner <calebq23@gmail.com>
2022-08-07 09:07:07 -0500

    UM. MORE STUFF??

--- handler.go
+++ handler.go
@@ -1,15 +1,35 @@
 package stupid
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Handler struct {
-	userDB           string
-	userCollection   string
-	uploadDB         string
-	uploadCollection string
-	dataDB           string
-	dataCollection   string
-	userCount        bool
+	mainDB   *mongo.Database
+	usersCol *mongo.Collection
 }
 
-func (h Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {}
+func (h Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "":
+		fallthrough
+	case "GET":
+	}
+}
+
+func (h Handler) GetCapabilities(resp http.ResponseWriter) {}
+
+func (h Handler) GetUserCount(resp http.ResponseWriter) {
+	i, err := h.usersCol.EstimatedDocumentCount(context.TODO())
+	if err != nil {
+		fmt.Println(err)
+		resp.Write([]byte("FAILURE: Can't get user count"))
+		return
+	}
+	resp.Write([]byte(strconv.Itoa(int(i))))
+}
