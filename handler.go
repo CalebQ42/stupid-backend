301f58e24c0004dc5f3f171800302b0900bafd48
Caleb Gardner <calebq23@gmail.com>
2022-08-11 03:37:31 -0500

    more stuff

--- handler.go
+++ handler.go
@@ -21,6 +21,8 @@ type Backend struct {
 
 // Implementation of http.Handler
 func (b Backend) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.EscapedPath())
+	fmt.Println(req.URL.EscapedFragment())
 	if req.Method == "GET" && req.URL.EscapedPath() == "/?userCount" {
 		count, err := b.AppUserCount()
 		if err != nil {
