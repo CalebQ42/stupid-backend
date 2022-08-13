3e2e49ceabefebd5461c4b6355aa7124f4d7551c
Caleb Gardner <calebq23@gmail.com>
2022-08-13 06:21:16 -0500

    More fixes

--- structs.go
+++ structs.go
@@ -1,13 +1,11 @@
 package stupid
 
 type ApiKey struct {
-	ID       string `json:"_id"`
 	Features string `json:"features"`
 	Death    int64  `json:"death"`
 }
 
 type GlobalUser struct {
-	ID          string `json:"_id"`
 	Username    string `json:"username"`
 	Password    string `json:"password"`
 	Email       string `json:"email"`
@@ -16,21 +14,18 @@ type GlobalUser struct {
 }
 
 type AppUser struct {
-	ID            string `json:"_id"`
-	Global        bool   `json:"hasGlobal"`
-	LastConnected int    `json:"lastConnected"`
+	Global        bool `json:"hasGlobal"`
+	LastConnected int  `json:"lastConnected"`
 }
 
 type AppData struct {
 	Data any    `json:"data"`
-	ID   string `json:"_id"`
 	Name string `json:"displayName"`
 	Type string `json:"type"`
 }
 
 type UserData struct {
 	Data       any
-	ID         string   `json:"_id"`
 	Owner      string   `json:"owner"`
 	Read       []string `json:"readPerm"`
 	Write      []string `json:"writePerm"`
@@ -38,12 +33,10 @@ type UserData struct {
 }
 
 type Report struct {
-	ID     string `json:"_id"`
 	Stack  string `json:"stack"`
 	Action string `json:"action"`
 }
 
 type Crash struct {
-	ID      string   `json:"_id"`
 	Reports []Report `json:"reports"`
 }
