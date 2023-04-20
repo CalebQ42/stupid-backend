10d06cc820fab0dd0becdbf6f0b16647ba8671e1
Caleb Gardner <calebq23@gmail.com>
2023-04-20 02:36:43 -0500

    Allow setting headers on all responses

--- stupid.go
+++ stupid.go
@@ -18,10 +18,12 @@ type Stupid struct {
 	Apps func(appID string) App
 
 	createUserMutex *sync.Mutex
+	headerValues    map[string]string
 	userPriv        ed25519.PrivateKey
 	userPub         ed25519.PublicKey
 }
 
+// Creates a new *Stupid.
 func NewStupidBackend(keyTable db.Table, apps func(appID string) App) *Stupid {
 	return &Stupid{
 		keys:            keyTable,
@@ -30,12 +32,19 @@ func NewStupidBackend(keyTable db.Table, apps func(appID string) App) *Stupid {
 	}
 }
 
+// Adds header values to add to every response (such as Access-Control-Allow-Origin).
+func (s *Stupid) SetHeaderValues(values map[string]string) {
+	s.headerValues = values
+}
+
+// Enables user authentication and creation.
 func (s *Stupid) EnableUserAuth(userTable db.UserTable, pubKey ed25519.PublicKey, privKey ed25519.PrivateKey) {
 	s.users = userTable
 	s.userPub = pubKey
 	s.userPriv = privKey
 }
 
+// Satisfies http.Handler
 func (s *Stupid) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	req := &Request{
 		Body:   r.Body,
@@ -43,6 +52,11 @@ func (s *Stupid) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		Path:   strings.Split(strings.TrimPrefix(path.Clean(r.URL.Path), "/"), "/"),
 		Method: r.Method,
 		Resp:   w,
+	}
+	if s.headerValues != nil {
+		for k, v := range s.headerValues {
+			w.Header().Set(k, v)
+		}
 	}
 	if len(req.Path) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
