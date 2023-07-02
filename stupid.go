3462796796f35d917f52802443585675d93a24f0
Caleb Gardner <calebq23@gmail.com>
2023-07-02 07:49:25 -0500

    CORS!

--- stupid.go
+++ stupid.go
@@ -83,6 +83,12 @@ func (s *Stupid) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set(k, v)
 		}
 	}
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+		return
+	}
 	if len(req.Path) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		return
