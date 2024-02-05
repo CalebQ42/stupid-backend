af25cf9146854fc73dac4751bb7841c38e405151
Caleb Gardner <calebq23@gmail.com>
2024-02-05 11:43:24 -0600

    Added extras field to crashes

--- crash/crash.go
+++ crash/crash.go
@@ -1,11 +1,12 @@
 package crash
 
 type Individual struct {
-	ID       string `json:"id" bson:"_id"`
-	Error    string `json:"error" bson:"error"`
-	Platform string `json:"platform" bson:"platform"`
-	Stack    string `json:"stack" bson:"stack"`
-	Version  string `json:"version" bson:"version"`
+	Extras   map[string]any `json:"extras" bson:"extras"`
+	ID       string         `json:"id" bson:"_id"`
+	Error    string         `json:"error" bson:"error"`
+	Platform string         `json:"platform" bson:"platform"`
+	Stack    string         `json:"stack" bson:"stack"`
+	Version  string         `json:"version" bson:"version"`
 }
 
 type Group struct {
